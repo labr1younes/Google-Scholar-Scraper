c1895f50f12908efc2563ae11bfbb920ac964160
Younes <younesetud@gmail.com>
2024-02-18 00:12:12 +0100

    linked function

--- GUI/gss_gui.py
+++ GUI/gss_gui.py
@@ -1,6 +1,7 @@
-from PyQt5 import QtWidgets ,uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLineEdit, QTextEdit
 import sys
+# from ..Core import core as mycore
 
 
 class MainWindow(QMainWindow):
@@ -8,6 +9,27 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent)
         self.setFixedSize(700, 150)
         uic.loadUi('gss.ui', self)  # Load the .ui file
+
+        # define widgets
+        self.btn = self.findChild(QPushButton, "btn")
+        self.inputName = self.findChild(QLineEdit, "inputName")
+        self.inputURL = self.findChild(QTextEdit, "inputURL")
+
+        self.setWindowTitle("GSS")
+
+        # adding functions
+        self.btn.clicked.connect(self.myfunction)
+
+    def is_url(self, url):
+        return "scholar.google.com/citations?view_op=search_authors" in url
+
+    def myfunction(self):
+        myurl = str(self.inputURL.toPlainText())
+        myname = str(self.inputName.text())
+        print(myurl, " ------------ ", myname)
+        # if self.is_url(myurl):
+            # ss = mycore.convert_2_json(mycore.scrape_all_pages(myurl), myname)
+
 
 
 def main():
